6cf99a735ddba0e8cc947774627bcc2f5570f6ad
Sebastian Czarnecki <sebcza@outlook.com>
2018-03-20 20:52:54 +0100

    Added and configured swagger

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
  - Stwórz nowy katalog dla projektu 
  - Wejdź do katalogu 
  - Stwórz projekt WebAPI wpisz w konsole `dotnet new webapi`
+
+ # Krok 2  - dodanie swaggera
+
+ - Dodaj paczkę nugeta dla swaggera. Wpisz w konsole `dotnet add package Swashbuckle.AspNetCore --version 2.2.0`
+ - Zarejestruj service swaggera. Dodaj w `Startup.cs` w **ConfigureServices**  
+ ```
+ services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info{ Title = "Warsaw Stops API", Version = "v1" }));
+ ```  
+ - Skonfiguruj swaggera. Dodaj w `Startup.cs` w **Configure**.    
+ ```app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Warsaw Stops API"));
+```
+
+ - Zobacz rezultat: http://localhost:5000/swagger
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WarsawCore
 {
@@ -24,6 +25,8 @@ namespace WarsawCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "Warsaw Stops API", Version = "v1" }));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,9 @@ namespace WarsawCore
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Warsaw Stops API"));
         }
     }
 }
